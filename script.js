090f07753a6385e667d983b61002dd39222b0239
Francisco Aquino <faquino@nonlinear.ca>
2018-01-25 16:14:19 -0200

    More dots

--- script.js
+++ script.js
@@ -70,25 +70,8 @@
 
   // Event handling
   function addListeners() {
-      if(!('ontouchstart' in window)) {
-          window.addEventListener('mousemove', mouseMove);
-      }
       window.addEventListener('scroll', scrollCheck);
       window.addEventListener('resize', resize);
-  }
-
-  function mouseMove(e) {
-      var posx = posy = 0;
-      if (e.pageX || e.pageY) {
-          posx = e.pageX;
-          posy = e.pageY;
-      }
-      else if (e.clientX || e.clientY)    {
-          posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-          posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-      }
-      target.x = posx;
-      target.y = posy;
   }
 
   function scrollCheck() {
